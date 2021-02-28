d8dc350a5d94324918bf200a3752481035822352
Carly Peyton <carlypeyton@gmail.com>
2021-02-27 21:50:10 -0500

    Create functions to view all roles and all departments, to mensure new roles and departments can be added.

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 //Dependencies
-const inquirer = require("inquirer")
-const mysql = require("mysql")
-const consoleTable = require('console.table');
+const inquirer = require("inquirer");
+const mysql = require("mysql");
+const consoleTable = require("console.table");
 
 //Create connection to database
 const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "",
+    password: "Leelanau1993!", /////////////Change password and include
     database: "employee_trackerDB",
 });
 
+//Connect to database and log to console
+//Run function to prompt user
 connection.connect((err) => {
     if (err) throw err;
     console.log(`Connected as id ${connection.threadId}.`);
@@ -24,30 +26,23 @@ function promptUser() {
         {
             name: "choice",
             type: "list",
-            message: "What would you like to do?",
+            message: "Please select option from below list.",
             choices: [
                 "Add Employee.",
                 "Add Role.",
                 "Add Department.",
-                "View All Employees.",
-                "View Employees by Role.",
-                "View Employees by Department.",
                 "Update Employee.",
+                "View All Employees.",
+                "View All Employees by Role.",
+                "View All Employees by Department.",
+                "View All Roles.",
+                "View All Departments.",
                 "DONE.",
             ]
         }
     ]).then(function (answer) {
         //Use switch/case for each option
         switch (answer.choice) {
-            case "Add Employee.":
-                addEmployee();
-                break;
-            case "Add Role.":
-                addRole();
-                break;
-            case "Add Department.":
-                addDepartment();
-                break;
             case "View All Employees.":
                 viewEmployees();
                 break;
@@ -57,42 +52,34 @@ function promptUser() {
             case "View All Employees by Department.":
                 viewDepartments();
                 break;
+            case "Add Employee.":
+                addEmployee();
+                break;
+            case "Add Role.":
+                addRole();
+                break;
+            case "Add Department.":
+                addDepartment();
+                break;
             case "Update Employee.":
                 updateEmployee();
+                break;
+            case "View All Roles.":
+                viewAllRoles();
+                break;
+            case "View All Departments.":
+                viewAllDepartments();
                 break;
             case "DONE.":
                 endApp();
                 break;
-        };
-    });
-};
-
-//Add Employee
-const addEmployee = () => {
-    inquirer.prompt([
-        {
-            name: "firstname",
-            type: "input",
-            message: "Please Provide Title.",
-        },
-        {
-            name: "lastname",
-            type: "input",
-            message: "Please Provide Salary.",
-        },
-    ]).then(function (answer) {
-        const query = "INSERT INTO employee SET ?";
-        connection.query(query, {first_name: answer.firstname, last_name: answer.lastname}, (err, res) => {
-            if (err) throw err;
-            console.table(res);
-            promptUser();
-        });
+        }
     });
 };
 
 //View All Employees (Include Employee Info Including Name, Title, Salary, Department, Manager. Order by Employee ID.)
 const viewEmployees = () => {
-    const query = "SELECT employee.id AS ID, employee.first_name AS First_Name, employee.last_name AS Last_Name, role.title AS Title, department.name AS Department, role.salary AS Salary, CONCAT(manager.first_name, ' ', manager.last_name) AS Manager FROM employee LEFT JOIN employee manager on manager.id = employee.manager_id INNER JOIN role ON (role.id = employee.role_id) INNER JOIN department ON (department.id = role.department_id) ORDER BY employee.id";
+    const query = "SELECT employee.id AS ID, employee.first_name AS First_Name, employee.last_name AS Last_Name, role.title AS Title, department.name AS Department, role.salary AS Salary, CONCAT(manager.first_name, ' ', manager.last_name) AS Manager FROM employee LEFT JOIN employee manager on manager.id = employee.manager_id LEFT JOIN role ON (role.id = employee.role_id) LEFT JOIN department ON (department.id = role.department_id) ORDER BY employee.id";
     connection.query(query, (err, res) => {
         if (err) throw err;
         console.table(res);
@@ -102,7 +89,7 @@ const viewEmployees = () => {
 
 //View Employees by Role (Include Name and Role. Order by Role.)
 const viewRoles = () => {
-    const query = "SSELECT role.title AS Title, employee.id AS ID, employee.first_name AS First_Name, employee.last_name AS Last_Name, department.name AS Department FROM employee LEFT JOIN role ON (role.id = employee.role_id) LEFT JOIN department ON (department.id = role.department_id) ORDER BY role.title";
+    const query = "SELECT role.title AS Title, employee.id AS ID, employee.first_name AS First_Name, employee.last_name AS Last_Name, department.name AS Department FROM employee LEFT JOIN role ON (role.id = employee.role_id) LEFT JOIN department ON (department.id = role.department_id) ORDER BY role.title";
     connection.query(query, (err, res) => {
         if (err) throw err;
         console.table(res);
@@ -120,10 +107,36 @@ const viewDepartments = () => {
     });
 };
 
-// End 
-const endApp = () => {
-    connection.end(); 
+//View All Roles (Include Department for each Role)
+const viewAllRoles = () => {
+    const query = "SELECT * FROM role LEFT JOIN department ON (department.id = role.department_id)";
+    connection.query(query, (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        promptUser();
+    });
 };
+
+//View All Departments
+const viewAllDepartments = () => {
+    const query = "SELECT * FROM department";
+    connection.query(query, (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        promptUser();
+    });
+};
+
+//End Connection
+const endApp = () => {
+    connection.end();
+};
+
+
+
+
+
+
 
 
 
